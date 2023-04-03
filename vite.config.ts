851a551853059b0036a350d6ba68a0a85c201227
Anders Clark <anders.clark@gmail.com>
2023-04-03 07:57:10 +0200

    Add config for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,41 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), VitePWA({
+    includeAssets: ['vite.svg', 'vue-logo.png'],
+    registerType: 'autoUpdate',
+    injectRegister: 'auto',
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,ico,png,webp,avif,jpg,jpeg,svg,gif,mp4}'],
+      cleanupOutdatedCaches: false,
+      sourcemap: true,
+    },
+    devOptions: {
+      enabled: true
+    },
+    manifest: {
+      "name": "Vue PWA",
+      "short_name": "Vue PWA",
+      description: 'A simple way of making QR-codes',
+      
+      "icons": [
+        {
+          "src": "/android-chrome-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+        {
+          "src": "/android-chrome-256x256.png",
+          "sizes": "256x256",
+          "type": "image/png"
+        }
+      ],
+      "theme_color": "#646cff",
+      "background_color": "#646cff",
+      "display": "standalone"
+    }})
+  ],
 })
